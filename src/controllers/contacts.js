3a4c49f7b82b9b400dec754e50786875d833f212
Lena Bokhan <leliklena@i.ua>
2025-06-03 10:45:27 +0300

    delete try/catch

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -15,20 +15,16 @@ export const getContactsController = async (req, res) => {
   });
 };
 export const getContactByIdController = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      throw createHttpError(404, 'Contact not found');
-    }
-    res.json({
-      status: 200,
-      data: contact,
-      message: `Successfully found contact with id ${contactId}!`,
-    });
-  } catch (err) {
-    next(err);
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
   }
+  res.json({
+    status: 200,
+    data: contact,
+    message: `Successfully found contact with id ${contactId}!`,
+  });
 };
 export const deleteContactController = async (req, res) => {
   const { contactId } = req.params;
