fc46c5d100300e90f2fb54233e5584c011cb9a94
Lena Bokhan <leliklena@i.ua>
2025-06-03 19:08:00 +0300

    add validation

---
+++ src/middlewares/isValidId.js
@@ -0,0 +1,9 @@
+import { isValidObjectId } from 'mongoose';
+import createHttpError from 'http-errors';
+export const isValidId = (req, res, next) => {
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
+    return next(createHttpError.BadRequest('ID should be an objectId'));
+  }
+  next();
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,15 +8,36 @@ import {
 import express from 'express';
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 const router = Router();
 const jsonParser = express.json();
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-router.post('/contacts', jsonParser, ctrlWrapper(createContactController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
+router.post(
+  '/contacts',
+  jsonParser,
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 router.patch(
   '/contacts/:contactId',
   jsonParser,
+  validateBody(updateContactSchema),
+  isValidId,
   ctrlWrapper(patchContactController),
 );
 export default router;
