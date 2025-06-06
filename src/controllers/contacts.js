e71c98f572414072d60f828643f9afb6f127f537
Lena Bokhan <leliklena@i.ua>
2025-06-06 05:47:04 +0300

    fix response getContacts Controller

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -16,7 +16,7 @@ export const getContactsController = async (req, res) => {
   res.status(200).json({
     status: 200,
     message: 'Successfully found contacts!',
-    ...contacts,
+    data: { ...contacts },
   });
 };
 export const getContactByIdController = async (req, res, next) => {
